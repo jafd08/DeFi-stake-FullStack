6f14eb18527e04bba131d9fe48a0c0613d79be94
Jesus Fung <jesus.fung@sykes.com>
2022-05-30 23:01:00 -0600

    working on part 2 solidity -> min 13:19

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -8,7 +8,12 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract TokenFarm is Ownable {
     // mapping token address -> staker address -> amount
     mapping(address => mapping(address => uint256)) public stakingBalance;
+    mapping(address => uint256) public uniqueTokensStaked;
+    mapping(address => address) public tokenPriceFeedMapping;
+    
     address[] public stakers; // list of all stakers on our platform
+    
+    IERC20 public dappToken;
     // stake tokens
     // unstake tokens
     // issue tokens
@@ -16,10 +21,51 @@ contract TokenFarm is Ownable {
     // get eth value
     address[] public allowedTokens;
 
-    function issueTokens() public onlyOwner {
-        // issue tokens of all stakers
+    constructor(address _dappTokenAddress) public {
+        dappToken = IERC20(_dappTokenAddress);
+    }
+    function setPriceFeedContract(){
+        
+
     }
 
+    function issueTokens() public onlyOwner {
+        // issue tokens of all stakers
+        for (
+            uint256 stakersIndex = 0;
+            stakerIndex < stakers.length;
+            stakersIndex++
+        ) {
+            address recipient = stakers[stakersIndex];
+            // send them a token reward
+            // based on their total value locked
+            // min 13:11
+            uint256 userTotalValue = getUserTotalValue(recipient);
+            dappToken.transfer(recipient, ????) // how much?
+            
+        }
+    }
+
+    function getUserTotalValue(address _user) public view returns (uint256){
+        uint256 totalValue = 0;
+        require(uniqueTokensStaked[_user] > 0, "No tokens staked!");
+        for (uint256 allowedTokensIndex =0 ; allowedTokensIndex < allowedTokens.length; allowedTokensIndex++ ){
+            totalValue = totalValue + getUserSingleTokenValue(_user, allowedTokens[allowedTokensIndex]);
+        }
+    }
+    function getUserSingleTokenValue(address _user , address _token) public view returns(uint256){
+        // if we have 1 eth stakes -> $2000 = 2000 coins
+        // $1 at this moment = 1 coin
+        // 200 DAI = 200
+        if (uniqueTokensStaked[_user] <=0 ){
+            return 0;
+        }
+        getTokenValue(_token);
+    }
+
+    function getTokenValue(address _token) public view returns (uint256){
+        // priceFeedAddress
+    }
     function stakeTokens(uint256 _amount, address _token) public {
         require(_amount > 0, "Amount must be more than 0");
         // require(_token is allowed ??)
@@ -29,9 +75,17 @@ contract TokenFarm is Ownable {
         stakingBalance[_token][msg.sender] =
             stakingBalance[_token][msg.sender] +
             _amount; // add the amount of what they already staked
+        if (uniqueTokensStaked[msg.sender] == 1) {
+            // person has only 1 unique token
+            stakers.push(msg.sender); // add to our list of stakers
+        }
     }
 
-    function updateUniqueTokensStaked(address user, address token) internal {}
+    function updateUniqueTokensStaked(address user, address token) internal {
+        if (stakingBalance[_token][_user] <= 0) {
+            uniqueTokensStaked[_user] = updateUniqueTokensStaked[_user] + 1;
+        }
+    }
 
     function addAllowedTokens(address _token) public onlyOwner {
         allowedTokens.push(_token);
